4ecbd828d2ea9d7223293e4ea80520cf54731428
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-17 18:40:00 -0500

    added views and routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,23 @@
+
+
 get '/' do
   # Look in app/views/index.erb
   erb :index
+end
+
+get '/cat' do
+  erb :cat
+end
+
+get '/cat/post' do
+  erb :post
+end
+
+get '/create' do
+  erb :create
+end
+
+
+post '/create' do
+  redirect to ('/cat/post')
 end
