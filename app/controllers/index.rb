9e9644121c4cefb05cdc69419fb287ec55caf7f8
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-17 20:17:13 -0500

    Linked database and user interface

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,15 +1,21 @@
 
+# GET ====================================================
+
 
 get '/' do
+  @category_list = Category.all
   # Look in app/views/index.erb
   erb :index
 end
 
-get '/cat' do
+get '/cat/:name' do
+  @category = Category.where("name = '#{params[:name]}'").first
   erb :cat
 end
 
-get '/cat/post' do
+get '/cat/:name/:post_id' do
+  @category = Category.where("name = '#{params[:name]}'").first
+  @post = Post.find(params[:post_id])
   erb :post
 end
 
@@ -17,7 +23,14 @@ get '/create' do
   erb :create
 end
 
+# POST ====================================================
 
 post '/create' do
+  puts "Params:"
+  new_post = Post.new(params[:post])
+  category = Category.find_or_create_by_name(params[:category])
+  category.posts << new_post
+  new_post.save
+  
   redirect to ('/cat/post')
 end
