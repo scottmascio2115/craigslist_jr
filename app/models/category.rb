00c318dc0d07a2b80c816550fc812e6aaa93ed25
kmazanec <k.mazanec@gmail.com>
2013-09-18 00:11:36 -0500

    added some stylin

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ActiveRecord::Base
   # Remember to create a migration!
   has_many :posts
+  def post_count
+    self.posts.count
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :category
+  def date
+    full_date = self.created_at
+    "#{full_date.year}-#{full_date.month}-#{full_date.day}"
+  end
 end
